47529ea414359b4b3ac78948a182eefe9f851177
RikelmeINF <rikelme.inf@gmail.com>
2023-07-29 11:18:41 -0300

    Primerias modificações no aplicativo

--- app/src/main/java/Primeiro/Aplicativo/MainActivity.kt
+++ app/src/main/java/Primeiro/Aplicativo/MainActivity.kt
@@ -6,11 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import Primeiro.Aplicativo.ui.theme.RikelmeTheme
+import android.os.Message
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,25 +20,19 @@ class MainActivity : ComponentActivity() {
             RikelmeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Rikelme")
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun PrimeiraMensagem(message: String) {
+    Text(text = message)
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RikelmeTheme {
-        Greeting("Android")
+        PrimeiraMensagem(message = "Bem vindo ao meu primeiro aplicativo")
     }
 }
