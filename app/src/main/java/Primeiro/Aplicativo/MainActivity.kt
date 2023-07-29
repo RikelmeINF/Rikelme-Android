1c78569804f4043094fcc52267ba16a286bfeda7
RikelmeINF <rikelme.inf@gmail.com>
2023-07-29 11:54:50 -0300

    Adicionando comentários e função de colunas e FontSize

--- app/src/main/java/Primeiro/Aplicativo/MainActivity.kt
+++ app/src/main/java/Primeiro/Aplicativo/MainActivity.kt
@@ -1,5 +1,6 @@
-package Primeiro.Aplicativo
+package com.exemple.Primeiro.Aplicativo
 
+import androidx.compose.foundation.layout.Row
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import Primeiro.Aplicativo.ui.theme.RikelmeTheme
 import android.os.Message
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,19 +23,23 @@ class MainActivity : ComponentActivity() {
             RikelmeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    PrimeiraMensagem(message = "Bem vindo ao meu primeiro aplicativo", por = "rikelme")
                 }
             }
         }
     }
 }
 @Composable
-fun PrimeiraMensagem(message: String) {
-    Text(text = message)
+fun PrimeiraMensagem(message: String, por: String) { //aqui crio uma função e entre "()" defino que tipo de dados seram recebidos
+    Column(content = { //aqui defino o formato que apareceram o texto na tela, seja coluna, lado a lado e etc
+        Text(text = message, fontSize = 36.sp) //aqui defino o texto que vai ser recebido atravez da "message" e o tamanho atraves do "fontsize"
+        Text(text = por, fontSize = 24.sp)
+    })
 }
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PrimeiraMensagemPreview() { //nesta linha é onde faço a chamada de visualização do aplicativo
     RikelmeTheme {
-        PrimeiraMensagem(message = "Bem vindo ao meu primeiro aplicativo")
+        PrimeiraMensagem(message = "Bem vindo ao meu primeiro aplicativo", por = "Rikelme") //chamo a função text e insiro a "message"
     }
 }
